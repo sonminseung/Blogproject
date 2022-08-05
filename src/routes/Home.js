a395875efbfaca04d82109706a78d42c75cdb692
sonminseung <105152528+sonminseung@users.noreply.github.com>
2022-08-05 16:44:27 +0900

    1 commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,20 +5,22 @@ function Home() {
         <div className="d-flex flex-column justify-content-center w-100 h-100">
             <div className="d-flex flex-column justify-content-center align-items-center">
         <div>
-            <h1 className="fw-light text-white m-0"><Link to="/">My Blog</Link></h1>
+            <h1 className="fw-light text-white m-0"><Link to="/"style={{ textDecoration: 'none',color:"black"}}>My Blog</Link></h1>
+            </div>
+                <div class="Menu-container">
+            <div>
+                <h3 className="fw-light text-white m-0"><Link to="/Project"style={{ textDecoration: 'none' ,color:"black" }}>My Project</Link></h3>
             </div>
             <div>
-                <h3 className="fw-light text-white m-0"><Link to="/Project">My Project</Link></h3>
+                <h3 className="fw-light text-white m-0"><Link to="/Profile"style={{ textDecoration: 'none',color:"black" }}>My Profile</Link></h3>
             </div>
             <div>
-                <h3 className="fw-light text-white m-0"><Link to="/Profile">My Profile</Link></h3>
+                <h3 className="fw-light text-white m-0"><Link to="/post"style={{ textDecoration: 'none' ,color:"black"}}>My post</Link></h3>
             </div>
             <div>
-                <h3 className="fw-light text-white m-0"><Link to="/post">My post</Link></h3>
+                <h3 className="fw-light text-white m-0"><Link to="/login"style={{ textDecoration: 'none',color:"black" }}>Login</Link></h3>
             </div>
-            <div>
-                <h3 className="fw-light text-white m-0"><Link to="/login">Login</Link></h3>
-            </div>
+                </div>
         </div>
         </div>
 
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,12 +1,23 @@
-import {useState} from "react";
+import {useEffect, useState, useHistory} from "react";
 import "./style.css";
 
 function Loginfunction() {
+    const getUser=async()=>{
+        const user_json = await (
+            await fetch(
+                `http://localhost:3000/user`
+            )
+        ).json();
+        Setdbusername(user_json.user.username)
+        Setdbpassword(user_json.user.username)
+    }
+    const history=useHistory()
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [username, Setusername] = useState("");
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [password, Setpassword] = useState("");
-
+    const [dbusername, Setdbusername]=useState("")
+    const [dbpassword, Setdbpassword]=useState("")
     const onChange_username = (e)=> {
         Setusername(e.target.value)
     };
@@ -15,13 +26,23 @@ function Loginfunction() {
     };
     const onSubmit=(e)=>{
         e.preventDefault()
+        if(dbusername===username && dbpassword=== password);
+        {
+            history.replace("/")
+        }
+        else
+        {
+            catch(error){
+                window.alert(error);
+            }
+        }
     };
     return (
-        <div className="d-flex flex-column justify-content-center w-100 h-100">
-            <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex flex-column justify-content-center w-100 h-100" class="login_box">
+            <div className="d-flex flex-column justify-content-center align-items-center" class="login_container">
             <form onSubmit={onSubmit}>
             <div>
-                <label for="username">username</label>
+                <label for="username" >username</label>
                 <input value={username}
                        type="text"
                        placeholder="enter your username"
@@ -36,9 +57,10 @@ function Loginfunction() {
                        onChange={onChange_password}/>
             </div>
 
-            <div>
+            <div class="login_button">
                 <button>Login</button>
             </div>
+
             </form>
         </div>
         </div>
