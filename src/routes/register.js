9bf650f62c60659d199bf6921241738e8e4f349a
sonminseung <105152528+sonminseung@users.noreply.github.com>
2022-08-08 21:11:44 +0900

    1 commit

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,5 +1,7 @@
 import {useState} from "react";
 import "./style.css"
+import {Link} from "react-router-dom";
+import home from "./Home";
 function Register() {
     const [Id, SetId]=useState("");
     const [Password, SetPassword]=useState("");
@@ -28,23 +30,28 @@ function Register() {
         e.preventDefault();
     }
         return (<div>
-        <form onSubmit={onSubmit}>
             <div>
-            <input value={Id} type="text" onChange={onId} placeholder="ID"/>
+               <h1> <Link to="/"style={{ textDecoration: 'none',color:"black"}}>My Blog</Link></h1>
+            </div>
+            <div className="register_container">
+        <form onSubmit={onSubmit} >
+            <div>
+            <input value={Id} type="text" onChange={onId} placeholder="ID" style={{marginTop:"4rem"}}/>
             </div>
             <div>
-        <input value={Password} type="password" onChange={onPassword} placeholder="Password"/>
+        <input value={Password} type="password" onChange={onPassword} placeholder="Password" style={{marginTop:"4rem"}}/>
             </div>
             <div>
-        <input value={ConfirmPassword} type="password" onChange={onConfirmPassword} placeholder="ConfirmPassword"/>
+        <input value={ConfirmPassword} type="password" onChange={onConfirmPassword} placeholder="ConfirmPassword" style={{marginTop:"4rem"}}/>
             </div>
 
             <div>
-        <input value={email} type="email"  onChange={onemail} placeholder="Email"/>
+        <input value={email} type="email"  onChange={onemail} placeholder="Email" style={{marginTop:"4rem"}}/>
             </div>
-            <button type="submit" value="register"/>
+            <button type="submit"  className="register_button" style={{margin:'2rem', width:'6rem', height:'3rem'}}>register</button>
     </form>
     </div>
+            </div>
     )
 }
 
